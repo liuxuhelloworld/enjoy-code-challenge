45ec6eaff5fd719646115ee6c07498a12d75cf66
liuxu <liuxuhelloworld@126.com>
2020-11-11 12:57:08 +0800

    leetcode, problem 12, integer to Roman, version 2 solution

--- src/leetcode/Problem12_IntegerToRoman.java
+++ src/leetcode/Problem12_IntegerToRoman.java
@@ -1,5 +1,11 @@
 package leetcode;
 
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 /**
  * https://leetcode-cn.com/problems/integer-to-roman/
  */
@@ -10,6 +16,9 @@ public class Problem12_IntegerToRoman {
 		{"", "C", "CC", "CCC", "CD", "D", "DC", "DCC", "DCCC", "CM"},
 		{"", "M", "MM", "MMM"}
 	};
+
+	private static int[] val = {1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
+	private static String[] symbol = {"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
 
 	public String intToRoman(int num) {
 		StringBuilder builder = new StringBuilder();
@@ -30,12 +39,30 @@ public class Problem12_IntegerToRoman {
 		return builder.toString();
 	}
 
+	public String intToRomanV2(int num) {
+		StringBuilder builder = new StringBuilder();
+
+		int i = 0;
+		while (num != 0) {
+			int quotient = num / val[i];
+			if (quotient != 0) {
+				for (int j = 0; j < quotient; j++) {
+					builder.append(symbol[i]);
+				}
+			}
+			num = num % val[i];
+			i++;
+		}
+
+		return builder.toString();
+	}
+
 	public static void main(String[] args) {
 		Problem12_IntegerToRoman obj = new Problem12_IntegerToRoman();
-//		System.out.println(obj.intToRoman(3));
-//		System.out.println(obj.intToRoman(4));
-//		System.out.println(obj.intToRoman(9));
-//		System.out.println(obj.intToRoman(58));
-		System.out.println(obj.intToRoman(1994));
+		System.out.println(obj.intToRomanV2(3));
+		System.out.println(obj.intToRomanV2(4));
+		System.out.println(obj.intToRomanV2(9));
+		System.out.println(obj.intToRomanV2(58));
+		System.out.println(obj.intToRomanV2(1994));
 	}
 }
