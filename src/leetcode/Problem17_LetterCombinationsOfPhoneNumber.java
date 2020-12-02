50294f4122f51c5f88c6a57e40bffce188836314
liuxu <liuxuhelloworld@126.com>
2020-12-02 12:49:26 +0800

    leetcode problem 17, letter combinations

--- src/leetcode/Problem17_LetterCombinationsOfPhoneNumber.java
+++ src/leetcode/Problem17_LetterCombinationsOfPhoneNumber.java
@@ -6,16 +6,16 @@ import java.util.*;
  * https://leetcode-cn.com/problems/letter-combinations-of-a-phone-number/
  */
 public class Problem17_LetterCombinationsOfPhoneNumber {
-	private static final Map<Character, Character[]> PHONE_NUMBER_TO_CHARACTERS = new HashMap<Character, Character[]>()
+	private static final Map<Character, String> PHONE_NUMBER_TO_CHARACTERS = new HashMap<Character, String>()
 	{{
-		put('2', new Character[] {'a', 'b', 'c'});
-		put('3', new Character[] {'d', 'e', 'f'});
-		put('4', new Character[] {'g', 'h', 'i'});
-		put('5', new Character[] {'j', 'k', 'l'});
-		put('6', new Character[] {'m', 'n', 'o'});
-		put('7', new Character[] {'p', 'q', 'r', 's'});
-		put('8', new Character[] {'t', 'u', 'v'});
-		put('9', new Character[] {'w', 'x', 'y', 'z'});
+		put('2', "abc");
+		put('3', "def");
+		put('4', "ghi");
+		put('5', "jkl");
+		put('6', "mno");
+		put('7', "pqrs");
+		put('8', "tuv");
+		put('9', "wxyz");
 	}};
 
 	public List<String> letterCombinations(String digits) {
@@ -36,20 +36,46 @@ public class Problem17_LetterCombinationsOfPhoneNumber {
 			combinations.add(combination.toString());
 		} else {
 			char digit = digits.charAt(index);
-			Character[] chars = PHONE_NUMBER_TO_CHARACTERS.get(digit);
-			for (int i = 0; i < chars.length; i++) {
-				combination.append(chars[i]);
+			String chars = PHONE_NUMBER_TO_CHARACTERS.get(digit);
+			for (int i = 0; i < chars.length(); i++) {
+				combination.append(chars.charAt(i));
 				backtrack(combinations, digits, index+1, combination);
 				combination.deleteCharAt(index);
 			}
 		}
 	}
 
+	public List<String> letterCombinationsV2(String digits) {
+		return letterCombinationRecur(digits, 0, digits.length()-1);
+	}
+
+	private List<String> letterCombinationRecur(String digits, int start, int end) {
+		if (start > end) {
+			return Collections.emptyList();
+		}
+
+		char digit = digits.charAt(start);
+		String chars = PHONE_NUMBER_TO_CHARACTERS.get(digit);
+		List<String> tails = letterCombinationRecur(digits, start + 1, end);
+		List<String> combinations = new ArrayList<>();
+		for (char ch : chars.toCharArray()) {
+			if (tails.size() > 0) {
+				for (String tail : tails) {
+					combinations.add(ch + tail);
+				}
+			} else {
+				combinations.add(ch + "");
+			}
+		}
+
+		return combinations;
+	}
+
 	public static void main(String[] args) {
 		Problem17_LetterCombinationsOfPhoneNumber obj = new Problem17_LetterCombinationsOfPhoneNumber();
-		System.out.println(obj.letterCombinations(""));
-		System.out.println(obj.letterCombinations("2"));
-		System.out.println(obj.letterCombinations("23"));
-		System.out.println(obj.letterCombinations("234"));
+		System.out.println(obj.letterCombinationsV2(""));
+		System.out.println(obj.letterCombinationsV2("2"));
+		System.out.println(obj.letterCombinationsV2("23"));
+		System.out.println(obj.letterCombinationsV2("234"));
 	}
 }
