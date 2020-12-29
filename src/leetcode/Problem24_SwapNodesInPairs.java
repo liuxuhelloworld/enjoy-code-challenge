62c9e05cd149b4e664cb814f38f8fba877f639c2
liuxu <liuxuhelloworld@126.com>
2020-12-29 12:30:21 +0800

    leetcode, problem 24, swap list nodes in pairs

---
+++ src/leetcode/Problem24_SwapNodesInPairs.java
@@ -0,0 +1,48 @@
+package leetcode;
+
+/**
+ * https://leetcode-cn.com/problems/swap-nodes-in-pairs/
+ */
+public class Problem24_SwapNodesInPairs {
+	public ListNode swapPairs(ListNode head) {
+		if (head == null) {
+			return null;
+		}
+
+		ListNode dummy = new ListNode(-1);
+		dummy.next = head;
+
+		ListNode swappedTail = dummy;
+
+		while (swappedTail.next != null) {
+			ListNode p, q;
+
+			p = swappedTail.next;
+			if (p.next == null) {
+				break;
+			} else {
+				q = p.next;
+			}
+
+			p.next = q.next;
+			q.next = p;
+			swappedTail.next = q;
+			swappedTail = p;
+		}
+
+		return dummy.next;
+	}
+
+	public static void main(String[] args) {
+		Problem24_SwapNodesInPairs obj = new Problem24_SwapNodesInPairs();
+		ListNode head = new ListNode(1);
+		head.next = new ListNode(2);
+		head.next.next = new ListNode(3);
+		head.next.next.next = new ListNode(4);
+		head.next.next.next.next = new ListNode(5);
+
+		head = obj.swapPairs(head);
+
+		System.out.println(head);
+	}
+}
