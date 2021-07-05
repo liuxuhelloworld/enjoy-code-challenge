8213607c4ee2014e495c4a7ba459cc99784f4180
liuxu <liuxuhelloworld@126.com>
2021-07-05 12:32:25 +0800

    binary tree traverse

---
+++ src/leetcode/tree/Traverse.java
@@ -0,0 +1,160 @@
+package leetcode.tree;
+
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Stack;
+
+public class Traverse {
+	public static void visit(TreeNode node) {
+		System.out.println(node.val);
+	}
+
+	public static void preOrderTraverse(TreeNode node) {
+		if (node == null) {
+			return;
+		}
+
+		visit(node);
+		preOrderTraverse(node.left);
+		preOrderTraverse(node.right);
+	}
+
+	public static void preOrderTraverseUsingStack(TreeNode node) {
+		if (node == null) {
+			return;
+		}
+
+		Stack<TreeNode> stack = new Stack<>();
+		stack.push(node);
+
+		while (!stack.isEmpty()) {
+			TreeNode cur = stack.pop();
+
+			visit(cur);
+
+			if (cur.right != null) {
+				stack.push(cur.right);
+			}
+
+			if (cur.left != null) {
+				stack.push(cur.left);
+			}
+		}
+	}
+
+	public static void inOrderTraverse(TreeNode node) {
+		if (node == null) {
+			return;
+		}
+
+		inOrderTraverse(node.left);
+		visit(node);
+		inOrderTraverse(node.right);
+	}
+
+	public static void inOrderTraverseUsingStack(TreeNode node) {
+		if (node == null) {
+			return;
+		}
+
+		Stack<TreeNode> stack = new Stack<>();
+		while (node != null
+			|| !stack.isEmpty()) {
+			while (node != null) {
+				stack.push(node);
+				node = node.left;
+			}
+
+			TreeNode cur = stack.pop();
+
+			visit(cur);
+
+			if (cur.right != null) {
+				node = cur.right;
+			}
+		}
+	}
+
+	public static void postOrderTraverse(TreeNode node) {
+		if (node == null) {
+			return;
+		}
+
+		postOrderTraverse(node.left);
+		postOrderTraverse(node.right);
+		visit(node);
+	}
+
+	public static void postOrderTraverseUsingStack(TreeNode node) {
+		if (node == null) {
+			return;
+		}
+
+		Stack<TreeNode> stack = new Stack<>();
+		stack.push(node);
+		TreeNode last = node;
+
+		while (!stack.isEmpty()) {
+			TreeNode cur = stack.peek();
+
+			boolean hasChild = (cur.left != null || cur.right != null);
+			boolean hasVisitAllChild = (last == cur.right || (last == cur.left && cur.right == null));
+
+			if (!hasChild || hasVisitAllChild) {
+				cur = stack.pop();
+				visit(cur);
+				last = cur;
+			} else {
+				if (cur.right != null) {
+					stack.push(cur.right);
+				}
+				if (cur.left != null) {
+					stack.push(cur.left);
+				}
+			}
+		}
+	}
+
+	public static void bfsTraverse(TreeNode node) {
+		if (node == null) {
+			return;
+		}
+
+		Queue<TreeNode> queue = new LinkedList<>();
+		queue.add(node);
+
+		while (!queue.isEmpty()) {
+			int size = queue.size();
+			while (size-- > 0) {
+				TreeNode cur = queue.poll();
+				visit(cur);
+				if (cur.left != null) {
+					queue.add(cur.left);
+				}
+				if (cur.right != null) {
+					queue.add(cur.right);
+				}
+			}
+		}
+	}
+
+	public static void main(String[] args) {
+		TreeNode root = new TreeNode(3);
+		TreeNode node1 = new TreeNode(1);
+		TreeNode node2 = new TreeNode(5);
+		TreeNode node3 = new TreeNode(4);
+		TreeNode node4 = new TreeNode(6);
+
+		root.left = node1;
+		root.right = node2;
+		node2.left = node3;
+		node2.right = node4;
+
+		inOrderTraverseUsingStack(root);
+		preOrderTraverse(root);
+		inOrderTraverse(root);
+		postOrderTraverse(root);
+		bfsTraverse(root);
+
+	}
+}
