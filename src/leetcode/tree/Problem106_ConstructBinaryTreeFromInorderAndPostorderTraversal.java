a2b0c0923498479cc792440a21de51b86a194b90
liuxu <liuxuhelloworld@126.com>
2021-07-20 12:57:58 +0800

    leetcode, problem 106, construct binary tree from inorder and postorder traversal

---
+++ src/leetcode/tree/Problem106_ConstructBinaryTreeFromInorderAndPostorderTraversal.java
@@ -0,0 +1,52 @@
+package leetcode.tree;
+
+/**
+ * https://leetcode-cn.com/problems/construct-binary-tree-from-inorder-and-postorder-traversal/
+ */
+public class Problem106_ConstructBinaryTreeFromInorderAndPostorderTraversal {
+	public TreeNode buildTree(int[] inorder, int[] postorder) {
+		return recurBuildTree(inorder, 0, inorder.length - 1,
+			postorder, 0, postorder.length - 1);
+	}
+
+	private TreeNode recurBuildTree(int[] inorder, int inStart, int inEnd,
+	                                int[] postorder, int postStart, int postEnd) {
+		if (postStart > postEnd) {
+			return null;
+		}
+
+		int val = postorder[postEnd];
+		TreeNode root = new TreeNode(val);
+
+		if (postStart == postEnd) {
+			return root;
+		}
+
+		int index = indexOf(inorder, inStart, inEnd, val);
+		int leftTreeNodesNum = index - inStart;
+		root.left = recurBuildTree(inorder, inStart, index - 1,
+			postorder, postStart, postStart + leftTreeNodesNum - 1);
+		root.right = recurBuildTree(inorder, index + 1, inEnd,
+			postorder, postStart + leftTreeNodesNum, postEnd - 1);
+
+		return root;
+	}
+
+	private int indexOf(int[] arr, int start, int end, int target) {
+		for (int i = start; i <= end; i++) {
+			if (arr[i] == target) {
+				return i;
+			}
+		}
+
+		return -1;
+	}
+
+	public static void main(String[] args) {
+		Problem106_ConstructBinaryTreeFromInorderAndPostorderTraversal obj =
+			new Problem106_ConstructBinaryTreeFromInorderAndPostorderTraversal();
+
+		TreeNode root = obj.buildTree(new int[] {1,9,2,3,15,20,7}, new int[] {1,2,9,15,7,20,3});
+		System.out.println(root);
+	}
+}
