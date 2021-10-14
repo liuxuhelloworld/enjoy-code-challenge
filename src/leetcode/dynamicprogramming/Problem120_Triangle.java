46d740e2e843dbdd67ecef25af4b230a84b46c87
liuxu <liuxuhelloworld@126.com>
2021-10-14 12:55:11 +0800

    leetcode, problem 120, triangle minimum path sum

--- src/leetcode/dynamicprogramming/Problem120_Triangle.java
+++ src/leetcode/dynamicprogramming/Problem120_Triangle.java
@@ -1,2 +1,54 @@
-package leetcode.dynamicprogramming;public class Problem120_Triangle {
+package leetcode.dynamicprogramming;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * https://leetcode-cn.com/problems/triangle/
+ */
+public class Problem120_Triangle {
+	public int minimumTotal(List<List<Integer>> triangle) {
+		int rows = triangle.size();
+
+		int[] dp = new int[rows + 1];
+		for (int i = 0; i <= rows; i++) {
+			dp[i] = Integer.MAX_VALUE;
+		}
+
+		List<Integer> first = triangle.get(0);
+		dp[1] = first.get(0);
+
+		for (int i = 1; i < rows; i++) {
+			List<Integer> cur = triangle.get(i);
+
+			for (int j = i + 1; j >= 1; j--) {
+				dp[j] = Math.min(dp[j], dp[j-1]) + cur.get(j-1);
+			}
+		}
+
+		int min = dp[1];
+		for (int i = 1; i <= rows; i++) {
+			if (dp[i] < min) {
+				min = dp[i];
+			}
+		}
+
+		return min;
+	}
+
+	public static void main(String[] args) {
+		Problem120_Triangle obj = new Problem120_Triangle();
+
+		List<List<Integer>> triangle = new ArrayList<>();
+		triangle.add(Arrays.asList(2));
+		triangle.add(Arrays.asList(3, 4));
+		triangle.add(Arrays.asList(6, 5, 7));
+		triangle.add(Arrays.asList(4, 1, 8, 3));
+		System.out.println(obj.minimumTotal(triangle));
+
+		triangle = new ArrayList<>();
+		triangle.add(Arrays.asList(-10));
+		System.out.println(obj.minimumTotal(triangle));
+	}
 }
