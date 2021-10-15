930645723a07670aeb2f6699b783b245050fa192
liuxu <liuxuhelloworld@126.com>
2021-10-15 12:58:03 +0800

    leetcode, problem 191 and 231, bit operations

--- src/leetcode/bit/Problem231_PowerOfTwo.java
+++ src/leetcode/bit/Problem231_PowerOfTwo.java
@@ -1,2 +1,10 @@
-package leetcode.bit;public class Problem231_PowerOfTwo {
+package leetcode.bit;
+
+/**
+ * https://leetcode-cn.com/problems/power-of-two/
+ */
+public class Problem231_PowerOfTwo {
+	public boolean isPowerOfTwo(int n) {
+		return n > 0 && (n & (n-1)) == 0;
+	}
 }
