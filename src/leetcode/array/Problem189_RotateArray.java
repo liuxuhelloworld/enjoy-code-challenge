a9f1dfdac00cb8b38817c8bc84182359b0d3a436
liuxu <liuxuhelloworld@126.com>
2022-02-21 11:32:11 +0800

    leetcode, problem 189, rotate array

--- src/leetcode/array/Problem189_RotateArray.java
+++ src/leetcode/array/Problem189_RotateArray.java
@@ -4,11 +4,17 @@ package leetcode.array;
  * https://leetcode-cn.com/problems/rotate-array/
  */
 public class Problem189_RotateArray {
+	/**
+	 * k-step
+	 *
+	 * @param nums
+	 * @param k
+	 */
 	public void rotate(int[] nums, int k) {
-		assert nums.length > 0;
-		assert k >= 0;
-
 		int len = nums.length;
+		if (len == 0) {
+			return;
+		}
 
 		k = k % len;
 		if (k == 0) {
@@ -29,8 +35,43 @@ public class Problem189_RotateArray {
 		}
 	}
 
+	/**
+	 * in-place reverse
+	 *
+	 * @param nums
+	 * @param k
+	 */
+	public void rotateV2(int[] nums, int k) {
+		int len = nums.length;
+		if (len == 0) {
+			return;
+		}
+
+		k = k % len;
+		if (k == 0) {
+			return;
+		}
+
+		// reverse left len-k elements
+		reverse(nums, 0, len - k - 1);
+
+		// reverse right k elements
+		reverse(nums, len - k, len - 1);
+
+		// reverse all elements
+		reverse(nums, 0, len - 1);
+	}
+
+	private void reverse(int[] nums, int left, int right) {
+		for (int i = left, j = right; i < j; i++, j--) {
+			int tmp = nums[i];
+			nums[i] = nums[j];
+			nums[j] = tmp;
+		}
+	}
+
 	public static void main(String[] args) {
 		Problem189_RotateArray obj = new Problem189_RotateArray();
-		obj.rotate(new int[] {1,2,3,4,5,6,7}, 3);
+		obj.rotateV2(new int[] {1,2,3,4,5,6,7}, 3);
 	}
 }
