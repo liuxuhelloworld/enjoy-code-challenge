76ac6fb1927945c6f139a48399ec532dba157df9
liuxu <liuxuhelloworld@126.com>
2022-05-10 19:21:31 +0800

    leetcode, problem 62, dynamic programming

--- src/leetcode/dynamicprogramming/Problem62_UniquePaths.java
+++ src/leetcode/dynamicprogramming/Problem62_UniquePaths.java
@@ -1,33 +1,10 @@
-package leetcode;
+package leetcode.dynamicprogramming;
 
 /**
  * https://leetcode-cn.com/problems/unique-paths/
  */
 public class Problem62_UniquePaths {
-	private int paths = 0;
-
 	public int uniquePaths(int m, int n) {
-		paths = 0;
-		backtrace(0, 0, m, n);
-		return paths;
-	}
-
-	private void backtrace(int curRow, int curCol, int m, int n) {
-		if (curRow == m-1 && curCol == n-1) {
-			paths++;
-			return;
-		}
-
-		if (curCol + 1 < n) {
-			backtrace(curRow, curCol+1, m, n);
-		}
-
-		if (curRow + 1 < m) {
-			backtrace(curRow + 1, curCol, m, n);
-		}
-	}
-
-	public int uniquePathsV2(int m, int n) {
 		int[][] dp = new int[m][n]; // dp[i][j] represents the unique paths from [0,0] to [i,j]
 		for (int i = 0; i < m; i++) {
 			dp[i][0] = 1;
@@ -50,6 +27,6 @@ public class Problem62_UniquePaths {
 
 	public static void main(String[] args) {
 		Problem62_UniquePaths obj = new Problem62_UniquePaths();
-		System.out.println(obj.uniquePathsV2(3, 7));
+		System.out.println(obj.uniquePaths(3, 7));
 	}
 }
