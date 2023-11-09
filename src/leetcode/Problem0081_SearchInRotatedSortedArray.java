61fcf2c260fbc396742333dda1c85a79fc1b0e7f
liuxu <liuxu@itiger.com>
2023-11-09 19:57:30 +0800

    leetcode, problem 81, search in rotated sorted array

--- src/leetcode/Problem0081_SearchInRotatedSortedArray.java
+++ src/leetcode/Problem0081_SearchInRotatedSortedArray.java
@@ -5,38 +5,34 @@ package leetcode;
  */
 public class Problem0081_SearchInRotatedSortedArray {
 	public boolean search(int[] nums, int target) {
-		int len = nums.length;
-		assert len > 0;
-
 		int left = 0, right = nums.length-1;
 
 		while (left <= right) {
 			int mid = left + (right-left)/2;
 
-			if (nums[mid] == target) {
+			if (nums[left] == target
+				|| nums[right] == target
+				|| nums[mid] == target) {
 				return true;
 			}
 
 			if (nums[mid] > nums[0]) {
-				// num[0..mid] is sorted
-				if (target >= nums[0] && target < nums[mid]) {
+				// num[left..mid] is no-decreasing sorted
+				if (target > nums[left] && target < nums[mid]) {
 					right = mid - 1;
 				} else {
 					left = mid + 1;
 				}
 			} else if (nums[mid] < nums[0]) {
-				if (target > nums[mid] && target <= nums[right]) {
+				// num[mid..right] is non-decreasing sorted
+				if (target > nums[mid] && target < nums[right]) {
 					left = mid + 1;
 				} else {
 					right = mid - 1;
 				}
 			} else {
-				for (int i = left; i <= right; i++) {
-					if (nums[i] == target) {
-						return true;
-					}
-				}
-				break;
+				left++;
+				right--;
 			}
 
 		}
