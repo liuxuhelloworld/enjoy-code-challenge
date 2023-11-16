d95bab765b0c0877ab8391ea5d91b661640d6cec
liuxu <liuxu@itiger.com>
2023-11-16 20:07:26 +0800

    leetcode, problem8, string to integer

--- src/leetcode/Problem0008_StringToInteger.java
+++ src/leetcode/Problem0008_StringToInteger.java
@@ -38,162 +38,16 @@ public class Problem0008_StringToInteger {
         } else if (ret < Integer.MIN_VALUE) {
             return Integer.MIN_VALUE;
         } else {
-            return (int)ret;
-        }
-    }
-
-    public int myAtoi2(String s) {
-        StatedString ss = new StatedString(s);
-        return ss.atoi();
-    }
-
-    private interface State {
-        void transfer(char ch);
-    }
-
-    private class StatedString {
-        private String s;
-
-        private State spaceState;
-        private State signState;
-        private State numberState;
-        private State endState;
-
-        private State state;
-
-        StatedString(String s) {
-            this.s = s;
-            this.spaceState = new SpaceState(this);
-            this.signState = new SignState(this);
-            this.numberState = new NumberState(this);
-            this.endState = new EndState(this);
-            this.state = spaceState;
-        }
-
-        public State getSpaceState() {
-            return spaceState;
-        }
-
-        public State getSignState() {
-            return signState;
-        }
-
-        public State getNumberState() {
-            return numberState;
-        }
-
-        public State getEndState() {
-            return endState;
-        }
-
-        public void setState(State state) {
-            this.state = state;
-        }
-
-        public int atoi() {
-            char[] chars = s.toCharArray();
-
-            boolean negative = false;
-            long ret = 0;
-            for (char ch : chars) {
-                state.transfer(ch);
-                if (state.equals(signState)) {
-                    negative = ch == '-' ? true : false;
-                } else if (state.equals(numberState)) {
-                    ret = ret * 10 + (ch - '0');
-                    if (ret > Integer.MAX_VALUE) {
-                        break;
-                    }
-                } else if (state.equals(endState)) {
-                    break;
-                }
-            }
-            ret = ret * (negative ? -1 : 1);
-
-            if (ret > Integer.MAX_VALUE) {
-                return Integer.MAX_VALUE;
-            } else if (ret < Integer.MIN_VALUE) {
-                return Integer.MIN_VALUE;
-            } else {
-                return (int)ret;
-            }
-        }
-    }
-
-    private class SpaceState implements State {
-        private StatedString ss;
-
-        SpaceState(StatedString ss) {
-            this.ss = ss;
-        }
-
-        @Override
-        public void transfer(char ch) {
-            if (Character.isSpaceChar(ch)) {
-                ss.setState(ss.getSpaceState());
-            } else if (ch == '+' || ch == '-') {
-                ss.setState(ss.getSignState());
-            } else if (Character.isDigit(ch)) {
-                ss.setState(ss.getNumberState());
-            } else {
-                ss.setState(ss.getEndState());
-            }
-        }
-    }
-
-    private class SignState implements State {
-        private StatedString ss;
-
-        SignState(StatedString ss) {
-            this.ss = ss;
-        }
-
-        @Override
-        public void transfer(char ch) {
-            if (Character.isDigit(ch)) {
-                ss.setState(ss.getNumberState());
-            } else {
-                ss.setState(ss.getEndState());
-            }
-        }
-    }
-
-    private class NumberState implements State {
-        private StatedString ss;
-
-        NumberState(StatedString ss) {
-            this.ss = ss;
-        }
-
-        @Override
-        public void transfer(char ch) {
-            if (Character.isDigit(ch)) {
-                ss.setState(ss.getNumberState());
-            } else {
-                ss.setState(ss.getEndState());
-            }
-        }
-    }
-
-    private class EndState implements State {
-        private StatedString ss;
-
-        EndState(StatedString ss) {
-            this.ss = ss;
-        }
-
-        @Override
-        public void transfer(char ch) {
-            ss.setState(ss.getEndState());
+            return (int) ret;
         }
     }
 
     public static void main(String[] args) {
         Problem0008_StringToInteger obj = new Problem0008_StringToInteger();
-        System.out.println(obj.myAtoi2("-13+8"));
-        System.out.println(obj.myAtoi2("42"));
-        System.out.println(obj.myAtoi2("   -42"));
-        System.out.println(obj.myAtoi2("4193 with words"));
-        System.out.println(obj.myAtoi2("words and 987"));
+        System.out.println(obj.myAtoi("-13+8"));
+        System.out.println(obj.myAtoi("42"));
+        System.out.println(obj.myAtoi("   -42"));
+        System.out.println(obj.myAtoi("4193 with words"));
+        System.out.println(obj.myAtoi("words and 987"));
     }
 }
