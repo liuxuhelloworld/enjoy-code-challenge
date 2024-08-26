63843972a103ef552110c09aa3c042d18b531955
liuxu <liuxuhelloworld@126.com>
2024-08-26 19:37:34 +0800

    leetcode, problem 1091. shortest path in binary matrix

--- src/leetcode/Problem1091_ShortestPathInBinaryMatrix.java
+++ src/leetcode/Problem1091_ShortestPathInBinaryMatrix.java
@@ -20,52 +20,41 @@ public class Problem1091_ShortestPathInBinaryMatrix {
 		boolean[][] visited = new boolean[n][n];
 
 		Queue<int[]> queue = new LinkedList<>();
+
 		queue.add(new int[] {0, 0});
 		visited[0][0] = true;
+		int depth = 0;
 
-		int path = 1;
 		while (!queue.isEmpty()) {
+			depth++;
+
 			int size = queue.size();
-			boolean flag = false;
-			for (int i = 0; i < size; i++) {
+			while (size-- > 0) {
 				int[] cur = queue.poll();
 				int row = cur[0], col = cur[1];
+
 				if (row == n-1 && col == n-1) {
-					return path;
+					return depth;
 				}
 
 				for (int k = 0; k < dx.length; k++) {
 					int newRow = row + dx[k];
 					int newCol = col + dy[k];
 
-					if (isValid(newRow, newCol, n)
-						&& grid[newRow][newCol] == 0
+					if (newRow < 0 || newRow >= n || newCol < 0 || newCol >= n) {
+						continue;
+					}
+
+					if (grid[newRow][newCol] == 0
 						&& !visited[newRow][newCol]) {
-						flag = true;
 						queue.add(new int[] {newRow, newCol});
 						visited[newRow][newCol] = true;
 					}
 				}
 			}
-
-			if (flag) {
-				path++;
-			}
 		}
 
 		return -1;
-	}
-
-	private boolean isValid(int row, int col, int n) {
-		if (row < 0 || row >= n) {
-			return false;
-		}
-
-		if (col < 0 || col >= n) {
-			return false;
-		}
-
-		return true;
 	}
 
 	public static void main(String[] args) {
